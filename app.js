860a48a1bafa6c64ea4f72b8ad9b8c61cc6f3f0d
Geoff Pleiss <pair+gpleiss@pivotallabs.com>
2014-05-21 12:37:25 -0400

    Rename environment variables.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , routes = require('./routes')
-  , config = require('./config/config')[process.env.ENV];
+  , config = require('./config/config')[process.env.ENVIRONMENT];
 
 var app = express();
 
