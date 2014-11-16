eb191ab1ae21205a6e70a854d0c34dd6a6f29a6c
Geoff Pleiss <gpleiss@gmail.com>
2014-11-16 10:45:38 -0500

    Remove testing code

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,6 @@ module.exports = function (grunt) {
       },
     },
 
-    exec: {
-      jasmine: {
-        command: 'node_modules/jasmine/bin/jasmine.js'
-      }
-    },
-
     watch: {
       web: {
         files: [
@@ -30,37 +24,15 @@ module.exports = function (grunt) {
           nospawn: true,
           atBegin: true,
         }
-      },
-      test: {
-        files: [
-          'spec/**/*.js',
-          'api/**/*.js',
-          'jakelib/**/*.js'
-        ],
-        tasks: [
-          'exec:jasmine'
-        ],
-        options: {
-          atBegin: true,
-        }
       }
     },
   });
 
   grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-exec');
 
   grunt.registerTask('web', 'launch webserver and watch tasks', [
     'watch:web'
-  ]);
-
-  grunt.registerTask('spec', 'run specs continuously', [
-    'watch:test'
-  ]);
-
-  grunt.registerTask('spec:ci', 'run specs once', [
-    'exec:jasmine'
   ]);
 
   grunt.registerTask('default', ['web']);
